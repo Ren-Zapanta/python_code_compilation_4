9dc7b4fec70a0cf2ce0b2ae0d80d6ddd294407ff
Ren <renzapnta4@gmail.com>
2025-03-30 01:58:20 +0800

    Verifies if the user_input ends with the suffix_input. Removes the suffix from the original string if yes. Prints the result.

--- batch_7/string_manipulation_remove_suffix.py
+++ batch_7/string_manipulation_remove_suffix.py
@@ -1,3 +1,8 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 string_input = input("Please enter a text: ") #Requests string input from the user
 suffix_input = input("What suffix would you like to be removed?: ") #Asks the user what suffix they would like to be removed
+
+if string_input.endswith(suffix_input): #Use endswith to verify if string_input ends with suffix_input
+    string_input = string_input[:-len(suffix_input)] #Removes the specified suffix from the original string input
+
+print(string_input) #Prints output
