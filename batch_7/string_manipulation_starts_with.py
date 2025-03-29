be360b33ef22484fe7dd365721eb3ca6a88fd4f1
Ren <renzapnta4@gmail.com>
2025-03-30 02:29:56 +0800

    Requests user input. Asks for prefix to check in the input. Gets the length of the prefix_input

---
+++ batch_7/string_manipulation_starts_with.py
@@ -0,0 +1,5 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+string_input = input("Please enter a statement: ") #Requests user input
+prefix_input = input("Enter a prefix for verification: ") #Asks for prefix to check
+
+prefix_length = len(prefix_input) #Gets the length of the preefix_input
