abea71c1d4b265fe63df52ce9c567cc0873dc132
Ren <renzapnta4@gmail.com>
2025-03-30 02:24:31 +0800

    Loops through eachh character in the input; verifying if all are lowercase. Prints message with its corresponding result from the casing verification

--- batch_7/string_manipulation_islower.py
+++ batch_7/string_manipulation_islower.py
@@ -3,5 +3,14 @@ user_input = input("Please enter a statement: ") #Requests user input
 
 assume_lowercase = True #Initial assumption of the input's casing
 
+for character in user_input: #Loops through all characters in the user_input
+    if "A" <= character <= "Z": #Verifies if the characters are in upper casing
+        assume_lowercase = False #Sets the initial assumption to false if uppercased letter is detected
+        break #Breaks loop
 
+#Prints corresponding message for each result
+if assume_lowercase == False: 
+    print("Your input is not in lowercase")
+else:
+    print("Your input is in lowercase")
 
