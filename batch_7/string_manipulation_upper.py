0199c7127cfddcec33fb4e7db6daa80ddd692c88
Ren <renzapnta4@gmail.com>
2025-03-30 02:01:15 +0800

    Requests input from the user. Initiatlizes empty string to store formatted text

---
+++ batch_7/string_manipulation_upper.py
@@ -0,0 +1,4 @@
+#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+user_input = input("Please enter a statement: ") #Requests input from the user
+
+formatted_text = "" #Initialize empty string to store formatted text
