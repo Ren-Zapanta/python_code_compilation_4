1d67e15bcc52beb71610e5f14f06b184818cf42a
Ren <renzapnta4@gmail.com>
2025-03-30 02:20:05 +0800

    Requests input from the user. Makes initial assumption that all characters in the input are in lowercase

---
+++ batch_7/string_manipulation_islower.py
@@ -0,0 +1,7 @@
+#Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+user_input = input("Please enter a statement: ") #Requests user input
+
+assume_lowercase = True #Initial assumption of the input's casing
+
+
+
