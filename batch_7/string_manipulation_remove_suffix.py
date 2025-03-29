e7a5b4642298cd91d81c7641cd3dc52b4eb5d5cd
Ren <renzapnta4@gmail.com>
2025-03-30 01:53:44 +0800

    Requests input from the user

---
+++ batch_7/string_manipulation_remove_suffix.py
@@ -0,0 +1,3 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+string_input = input("Please enter a text: ") #Requests string input from the user
+suffix_input = input("What suffix would you like to be removed?: ") #Asks the user what suffix they would like to be removed
