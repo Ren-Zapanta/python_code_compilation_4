a5784201634f5881754e0a93e9ce6afabe8eab3b
Ren <renzapnta4@gmail.com>
2025-03-30 02:15:26 +0800

    Uses ascii to convert lowercase letters to uppercase then prints the output

--- batch_7/string_manipulation_upper.py
+++ batch_7/string_manipulation_upper.py
@@ -2,3 +2,11 @@
 user_input = input("Please enter a statement: ") #Requests input from the user
 
 formatted_text = "" #Initialize empty string to store formatted text
+
+for character in user_input: #Loops through the characters in the user_input
+    if 'a' <= character <= 'z': #verifies if lowercase characters are present in the input
+        formatted_text += chr(ord(character) - 32) #Converts lower case letters to uppercase using ascii
+    else:
+        formatted_text += character #Keeps the already uppercase letters in the input
+
+print(formatted_text) #Prints output
