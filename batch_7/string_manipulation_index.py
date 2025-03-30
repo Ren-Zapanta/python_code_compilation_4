2eeb56326105bda2234e3c8d67e9a942e66af49a
Ren <renzapnta4@gmail.com>
2025-03-30 18:10:00 +0800

    Asks the user for string input. Asks the user which character they would like to know the first location of within the string

---
+++ batch_7/string_manipulation_index.py
@@ -0,0 +1,6 @@
+#Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+string_input = input("Please enter a statement: ") #Asks the user for string input
+character_to_find = input("What would you like to know the index of? ") #Asks the user which character they would like to know the position of 
+
+
+
