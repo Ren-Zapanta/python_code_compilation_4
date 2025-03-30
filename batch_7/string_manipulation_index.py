53a9f5a254e9cd28934b6c8a263c9c465b9cb1bf
Ren <renzapnta4@gmail.com>
2025-03-30 18:14:08 +0800

    Finds the first location of the character in the string andd prints the result

--- batch_7/string_manipulation_index.py
+++ batch_7/string_manipulation_index.py
@@ -2,5 +2,9 @@
 string_input = input("Please enter a statement: ") #Asks the user for string input
 character_to_find = input("What would you like to know the index of? ") #Asks the user which character they would like to know the position of 
 
+character_location = string_input.find(character_to_find) #Finds the first location of the character in the string
+
+print(f"'{character_to_find}' was first found at index: {character_location}") #Prints the output
+
 
 
