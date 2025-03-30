dda628365adef8210e36f42c607dbc7cb63c9573
Ren <renzapnta4@gmail.com>
2025-03-30 18:19:37 +0800

    Asks the user for a string. Asks the user which character within the string they would like to find.

---
+++ batch_7/string_manipulation_rindex.py
@@ -0,0 +1,4 @@
+#Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+string_input = input("Please enter a string: ") #Asks the user for a string
+character_to_find = input("Which character would you like to find? ") #Asks the user which character within the string they would like to find
+
