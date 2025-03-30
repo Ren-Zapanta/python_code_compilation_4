7cc4c425aa23e2e571ecf0ff023213b29fa2068b
Ren <renzapnta4@gmail.com>
2025-03-30 15:28:22 +0800

    concatenates padding to the original string. Prints it as its output

--- batch_7/string_manipulation_rjust.py
+++ batch_7/string_manipulation_rjust.py
@@ -4,6 +4,14 @@ desired_length = int(input("Enter length: ")) #Asks user how far to the right th
 
 current_length = len(string_input) #Gets the length of the string input
 
+if current_length < desired_length: #Checks if string input is shorter than the desired length
+    padding = " " * (desired_length - current_length) #Gets number of needed padding according to the difference of desired and current length
+    output_string = padding + string_input #Concatenates the padding to the original string_input; creating the output
+else:
+    output_string = string_input #Keeps the original input as the output if the current_length is greater than the desired_length
+
+print(f"'{output_string}'")#Prints the output. Uses single quotes to indicate spaces added to the string"
+
 
 
 
