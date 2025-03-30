a2e2cbe524b0275a209104bc2202c744ace33be1
Ren <renzapnta4@gmail.com>
2025-03-30 18:23:50 +0800

    Finds where the specified character last appears in the string then prints the location

--- batch_7/string_manipulation_rindex.py
+++ batch_7/string_manipulation_rindex.py
@@ -2,3 +2,6 @@
 string_input = input("Please enter a string: ") #Asks the user for a string
 character_to_find = input("Which character would you like to find? ") #Asks the user which character within the string they would like to find
 
+character_location = string_input.rindex(character_to_find) #Finds where the specified character last appears in the string
+
+print(f"'{character_to_find}' last appears on index: {character_location}") #Prints the result
