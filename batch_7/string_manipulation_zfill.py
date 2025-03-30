8ee58db7807f64ba8abd388d9b0b522f6f250153
Ren <renzapnta4@gmail.com>
2025-03-30 15:35:15 +0800

    Asks user for string and desired length for inputs. Gets the length of the string input.

---
+++ batch_7/string_manipulation_zfill.py
@@ -0,0 +1,7 @@
+#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+string_input = input("Please enter a statement: ") #Asks user for string input
+desired_length = int("Enter length: ") #Asks the user for desired string length
+
+length_of_input = len(string_input) #Gets the length of the string input
+
+
