96eccb1f71daff6b50de42886138f43c1762fd10
Ren <renzapnta4@gmail.com>
2025-03-30 17:46:46 +0800

    Loops through the whole string, extracting parts from it using the length of the substring input. Counts how many times that substring was found and prints it as an output.

--- batch_7/string_manipulation_count.py
+++ batch_7/string_manipulation_count.py
@@ -3,3 +3,11 @@ string_input = input("Please enter a text: ") #Asks the user for string input
 substring_input = input("Enter substring: ") #Asks the user for the substring to count
 
 substring_count = 0 #Initialize a count set to 0
+
+substring_length = len(substring_input) #Gets the length of the substring_input
+
+for i in range(len(string_input) - substring_length + 1): #Loops through the whole string_input
+    if string_input[i:i + substring_length] == substring_input: #Extracts a part of the string with the using the length of the substring and compares
+        substring_count += 1 #Adds to the count
+
+print(f"Your substring '{substring_input}' has been found {substring_count} times.") #Prints the output
