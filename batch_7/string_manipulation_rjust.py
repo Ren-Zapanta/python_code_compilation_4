b8669bd3c582caf393988df9b4a5b7e9be5da82b
Ren <renzapnta4@gmail.com>
2025-03-30 15:13:08 +0800

    Asks the user for a string input and their desired length. Gets the length of the input

---
+++ batch_7/string_manipulation_rjust.py
@@ -0,0 +1,12 @@
+#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+string_input = input("Please enter a statement: ") #Asks user for string input
+desired_length = int(input("Enter length: ")) #Asks user how far to the right they want their output to be
+
+current_length = len(string_input) #Gets the length of the string input
+
+
+
+
+
+
+
