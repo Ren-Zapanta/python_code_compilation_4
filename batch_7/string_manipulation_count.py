68084331053d50006adb667e3f165c2ff2c381a4
Ren <renzapnta4@gmail.com>
2025-03-30 16:54:32 +0800

    Asks the user for a string input and the substring to count. Initializes substring count set to 0.

---
+++ batch_7/string_manipulation_count.py
@@ -0,0 +1,5 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+string_input = input("Please enter a text: ") #Asks the user for string input
+substring_input = input("Enter substring: ") #Asks the user for the substring to count
+
+substring_count = 0 #Initialize a count set to 0
